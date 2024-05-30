451758866522a73226a20fb0eda78665680756ac
admin <rahulhacksgood@gmail.com>
2024-05-31 03:15:25 +0530

    rag

--- rag_part/app.py
+++ rag_part/app.py
@@ -7,3 +7,6 @@ from phi.document import Document
 from phi.document.reader.pdf import PDFReader
 from phi.document.reader.website import WebsiteReader
 from phi.utils.log import logger
+
+from assistant import get_auto_rag_assistant  # type: ignore
+from assistant import get_case_research_agent
