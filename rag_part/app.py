c6253fe6143e6bc126f0bc938757c08ebefbd780
admin <rahulhacksgood@gmail.com>
2024-05-31 03:15:14 +0530

    rag

--- rag_part/app.py
+++ rag_part/app.py
@@ -1,5 +1,9 @@
 import nest_asyncio
 from typing import List
 
-
-`
+import streamlit as st
+from phi.assistant import Assistant
+from phi.document import Document
+from phi.document.reader.pdf import PDFReader
+from phi.document.reader.website import WebsiteReader
+from phi.utils.log import logger
